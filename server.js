aca3100e063e2d1e8cf6ce2efd1a2bce2ff71098
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-02-28 14:38:43 -0800

    setting routes and modules files

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+dotenv.config();
+
+
+//Middleware
+
+
+//Routes
+
+
+// Global error handling
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
