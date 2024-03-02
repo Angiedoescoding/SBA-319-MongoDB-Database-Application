6774255eb3dde7b31fa8238ee6654c0dc6c97cb5
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-03-01 19:50:04 -0800

    data documents updated

--- utilities-data/employees.js
+++ utilities-data/employees.js
@@ -3,7 +3,9 @@
 const employeesData = [
     {id: 1, name: "Adam Smith", hireYear: 2020, title: "QA Engineer" },
     {id: 2, name: "Olivia Johnson", hireYear: 2017, title: "HR Specialist" },
-    {id: 3, name: "Julie Bolder", hireYear: 2022, title: "Payroll Specialist" }
+    {id: 3, name: "Julie Bolder", hireYear: 2022, title: "Payroll Specialist" },
+    {id: 4, name: "Denzel Washington", hireYear: 2011, title: "Chief of Finance" },
+    {id: 5, name: "Chris Evans", hireYear: 2008, title: "Captain America" },
 ];
 
 module.exports = employeesData;
