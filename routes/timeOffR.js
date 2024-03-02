828f9e0e7b5aa8b4432cd370f6b3b9569e10220f
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-03-01 22:19:47 -0800

    Adding index to the date field (issues)

--- routes/timeOffR.js
+++ routes/timeOffR.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { MongoClient } = require('mongodb');
+
 //const timeoffData = require('../utilities-data/timeOffRequests')
 
 
-// const timeOffRequests = [
+// Adding index to the date field (I am unable to solve the issues with conncetion in this case.)
+        // Terminal error: const client = new MongoClient.connect(process.env.ATLAS_URI); TypeError: MongoClient.connect is not a constructor
 
-//     { id: 1, date: "January 1, 2024", typeOffRequest: "Personal-Day", hoursADay: 8 },
-//     { id: 2, date: "March 25, 2024", typeOffRequest: "Vacation", hoursADay: 8 },
-//     { id: 3, date: "June 10, 2024", typeOffRequest: "Sick Leave", hoursADay: 3 }
-// ];
+const client = new MongoClient.connect(process.env.ATLAS_URI);      // error ˆ
+const db = client.db();
+timeOffRequests= db.collection('timeOffRequests');
+timeOffRequests.createIndex({ date: 1 }, (err, result) => {
+    if (err) {
+        console.log("Error creating index:", err);
+    } else {
+        console.log("Index created on the 'date' field in the 'timeOffRequests' collection", result);
+    }
+});
+
 
 const timeOffRequests = [
     { id: 1, date: new Date("2024-01-01"), typeOffRequest: "Personal-Day", hoursADay: 8 },
     { id: 2, date: new Date("2024-03-25"), typeOffRequest: "Vacation", hoursADay: 8 },
-    { id: 3, date: new Date("2024-06-10"), typeOffRequest: "Sick Leave", hoursADay: 3 }
+    { id: 3, date: new Date("2024-06-10"), typeOffRequest: "Sick Leave", hoursADay: 3 },
+    { id: 4, date: new Date("2024-06-10"), typeOffRequest: "Parental Leave", hoursADay: 8 },
+    { id: 5, date: new Date("2024-06-10"), typeOffRequest: "Holiday Deferral", hoursADay: 8 }
 ];
 
 // GET all time off requests
@@ -76,3 +88,14 @@ router.delete('/:id', (req, res) => {
 
 module.exports = router;
 
+
+
+// OLD
+
+
+// const timeOffRequests = [
+
+//     { id: 1, date: "January 1, 2024", typeOffRequest: "Personal-Day", hoursADay: 8 },
+//     { id: 2, date: "March 25, 2024", typeOffRequest: "Vacation", hoursADay: 8 },
+//     { id: 3, date: "June 10, 2024", typeOffRequest: "Sick Leave", hoursADay: 3 }
+// ];
